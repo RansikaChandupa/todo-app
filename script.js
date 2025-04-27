ff47874d871560ea51048230d9a965a7316aa509
RansikaChandupa <ransikachandupa@gmail.com>
2025-04-27 20:06:47 +0530

    Add JavaScript code(script.js)

--- script.js
+++ script.js
@@ -11,5 +11,27 @@ function addTask(){
         const li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+
+        const span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
+    saveTask();
+    inputBox.value = "";
 }
+listContainer.addEventListener("click", (e)=>{
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+    saveTask();
+})
+function saveTask(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTasks(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTasks();
